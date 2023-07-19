e64c46123a64ae403d27ee1cdeac45c1eda17b69
KatSchultz <ks.katieschultz@gmail.com>
2023-07-19 16:13:07 -0400

    removed counter references

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
 import matchReducer from "../features/matches/matchSlice";
 import gameCardReducer from "../features/gameCards/gameCardsSlice";
 import flippedCardsReducer from "../features/flippedCards/flippedCardsSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     matches: matchReducer,
     gameCards: gameCardReducer,
     flippedCards: flippedCardsReducer,
